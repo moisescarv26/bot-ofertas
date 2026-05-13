6a319aa2efce191892047d6eb83e8d234baa6daa
moisescarv26 <moises.c.f@hotmail.com>
2026-05-13 16:39:01 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,50 @@
 from telegram import Bot
 import asyncio
+import json
+import random
 
-TOKEN = "8780348557:AAFmjkBMxTYv8YnyUCkAyHQ8IbosVEPKPJo"
+TOKEN = "SEU_TOKEN"
 CHAT_ID = "-1003922054940"
 
 async def main():
 
     bot = Bot(token=TOKEN)
 
+    with open("produtos.json", "r", encoding="utf-8") as file:
+        produtos = json.load(file)
+
+    produto = random.choice(produtos)
+
+    desconto = round(
+        ((produto["preco_normal"] - produto["preco_promocao"]) /
+        produto["preco_normal"]) * 100
+    )
+
+    descricao = [
+        "🔥 Oferta por tempo limitado",
+        "⚡ Excelente custo-benefício",
+        "🚀 Produto muito procurado",
+        "💥 Vale a pena conferir",
+        "🎯 Ótima oportunidade"
+    ]
+
+    mensagem = f"""
+{random.choice(descricao)}
+
+📦 {produto['titulo']}
+
+💰 De R$ {produto['preco_normal']}
+🔥 Por R$ {produto['preco_promocao']}
+
+🎯 {desconto}% OFF
+
+🛒 Comprar:
+{produto['link']}
+"""
+
     await bot.send_message(
         chat_id=CHAT_ID,
-        text="🚀 BOT AUTOMÁTICO FUNCIONANDO PELO GITHUB"
+        text=mensagem
     )
 
 asyncio.run(main())
