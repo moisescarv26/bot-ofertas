474d923e0278a3c78830ec530c861823e0b46056
moisescarv26 <moises.c.f@hotmail.com>
2026-05-14 10:50:25 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,63 @@
 from telegram import Bot
 import asyncio
-import feedparser
+import json
+import random
 
 TOKEN = "8780348557:AAFmjkBMxTYv8YnyUCkAyHQ8IbosVEPKPJo"
 CHAT_ID = "-1003922054940"
 
 bot = Bot(token=TOKEN)
 
-RSS_URL = "https://www.promobit.com.br/feed/"
+descricoes = {
+    "Suplementos": [
+        "💪 Excelente para performance",
+        "🔥 Muito procurado nos treinos",
+        "⚡ Ótimo custo-benefício",
+        "🏋️ Excelente opção fitness"
+    ],
+
+    "Tecnologia": [
+        "🚀 Produto muito procurado",
+        "🔥 Excelente oportunidade",
+        "⚡ Tecnologia com ótimo preço",
+        "🎯 Vale a pena conferir"
+    ]
+}
+
+urgencia = [
+    "⏳ Oferta pode acabar rápido",
+    "🚨 Promoção relâmpago",
+    "⚡ Aproveite antes que aumente",
+    "🔥 Últimas unidades"
+]
 
 async def main():
 
-    feed = feedparser.parse(RSS_URL)
+    with open("produtos.json", "r", encoding="utf-8") as file:
+        produtos = json.load(file)
 
-    mensagem = "🔥 TESTE RSS\n\n"
+    produto = random.choice(produtos)
 
-    for item in feed.entries[:5]:
-        mensagem += f"{item.title}\n{item.link}\n\n"
+    desconto = round(
+        ((produto["preco_normal"] - produto["preco_promocao"]) /
+        produto["preco_normal"]) * 100
+    )
+
+    mensagem = f"""
+{random.choice(descricoes[produto["categoria"]])}
+
+📦 {produto["titulo"]}
+
+💰 De R$ {produto["preco_normal"]}
+🔥 Por R$ {produto["preco_promocao"]}
+
+🎯 {desconto}% OFF
+
+{random.choice(urgencia)}
+
+🛒 Comprar agora:
+{produto["link"]}
+"""
 
     await bot.send_message(
         chat_id=CHAT_ID,
