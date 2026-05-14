16bc512290c5bbfd779272a372a857426a9fabc4
moisescarv26 <moises.c.f@hotmail.com>
2026-05-14 15:31:11 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,83 @@
 from telegram import Bot
 import asyncio
 import json
+import random
 
 TOKEN = "8780348557:AAFmjkBMxTYv8YnyUCkAyHQ8IbosVEPKPJo"
 CHAT_ID = "-1003922054940"
 
-async def main():
+bot = Bot(token=TOKEN)
 
-    bot = Bot(token=TOKEN)
+descricoes = [
+    "🔥 Oferta encontrada automaticamente",
+    "⚡ Excelente oportunidade",
+    "💥 Produto em destaque",
+    "🚀 Vale a pena conferir"
+]
+
+urgencia = [
+    "⏳ Oferta pode acabar rápido",
+    "🚨 Promoção relâmpago",
+    "🔥 Aproveite antes que aumente",
+    "⚡ Últimas unidades"
+]
+
+hashtags = [
+    "#Amazon",
+    "#Promoção",
+    "#Ofertas",
+    "#Desconto"
+]
+
+async def main():
 
     with open("produtos.json", "r", encoding="utf-8") as file:
         produtos = json.load(file)
 
-    produto = produtos[0]
+    produto = random.choice(produtos)
 
-    mensagem = f"""
-🔥 TESTE AUTOMÁTICO
-
-📦 {produto["titulo"]}
-
-💰 De R$ {produto["preco_normal"]}
-🔥 Por R$ {produto["preco_promocao"]}
-
-🛒 Comprar:
-{produto["link"]}
-"""
-
-    # TESTE SEM IMAGEM PRIMEIRO
-    await bot.send_message(
-        chat_id=CHAT_ID,
-        text=mensagem
+    desconto = round(
+        ((produto["preco_antigo"] - produto["preco_atual"]) /
+        produto["preco_antigo"]) * 100
     )
 
-    print("Mensagem enviada com sucesso")
+    mensagem = f"""
+{random.choice(descricoes)}
+
+📦 {produto["nome"]}
+
+💰 De R$ {produto["preco_antigo"]}
+🔥 Por R$ {produto["preco_atual"]}
+
+🎯 {desconto}% OFF
+
+{random.choice(urgencia)}
+
+🛒 Comprar agora:
+{produto["link"]}
+
+{' '.join(random.sample(hashtags, 3))}
+"""
+
+    try:
+
+        await bot.send_photo(
+            chat_id=CHAT_ID,
+            photo=produto["imagem"],
+            caption=mensagem
+        )
+
+        print("Imagem enviada com sucesso")
+
+    except Exception as erro:
+
+        print("Erro imagem:", erro)
+
+        await bot.send_message(
+            chat_id=CHAT_ID,
+            text=mensagem
+        )
+
+        print("Mensagem enviada sem imagem")
 
 asyncio.run(main())
